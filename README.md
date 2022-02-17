44f009ba0907ed265d06d456d78b3840e00cd29c
dymafr <erwan1309@hotmail.com>
2022-02-17 12:03:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-sqtqmr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-sqtqmr)
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template></template>
+
+<script setup lang="ts">
+interface User {
+  name: string;
+  age: number | string;
+}
+
+let user: User | null;
+
+function createUser(name: string): User | null {
+  return {
+    name,
+    age: 18,
+  };
+}
+
+user = createUser('Paul');
+</script>
+
+<style></style>
